e3bed91b4d7c6948773ca54d3f3f48c935113db7
Demo User <demo@example.com>
2025-04-01 00:27:11 +0900

    Test pre-commit hook

---
+++ packages/control-plane/admin-console/test-formatting.js
@@ -0,0 +1,8 @@
+// This is a test file to verify that our pre-commit hook works correctly
+function testFunction() {
+  const unformattedVariable = 'This is not formatted correctly';
+
+  return unformattedVariable;
+}
+
+export default testFunction;
